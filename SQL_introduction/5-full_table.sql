b59a080c7e94ffcff65366add40e4d15d5a23c55
notReuel <northcottreuel@gmail.com>
2024-01-16 15:07:07 +0100

    messing arround

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,17 @@
--- A script to describe a table
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+-- Script to print the full description of the table first_table in hbtn_0c_0 database
+
+SELECT
+  CONCAT('CREATE TABLE `', table_name, '` (\n',
+         GROUP_CONCAT(
+           CONCAT('  `', column_name, '` ', column_type,
+                  IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
+                  IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', QUOTE(COLUMN_DEFAULT)), ''),
+                  IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', ''),
+                  ',')
+           SEPARATOR '\n'),
+         IF(CHARACTER_SET_NAME IS NOT NULL, CONCAT(') ENGINE=InnoDB DEFAULT CHARSET=', CHARACTER_SET_NAME, ';'), ')')
+       ) AS CreateTableStatement
+FROM information_schema.columns
+WHERE table_schema = 'hbtn_0c_0'
+  AND table_name = 'first_table'
+GROUP BY table_name;
