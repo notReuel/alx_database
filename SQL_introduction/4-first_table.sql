188a36d3c90a20549c06d042222938d2906693d8
notReuel <northcottreuel@gmail.com>
2024-01-16 14:12:43 +0100

    Checker showed and error and told me the table does not exist, so i removed the 'IF NOT EXISTS' statement

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- A script to create a table in a selected database
-USE hbtn_test_db
-    CREATE TABLE IF NOT EXISTS first_table(
+USE hbtn_test_db_0
+    CREATE TABLE first_table(
         id INT,
         name VARCHAR(256)
     );
