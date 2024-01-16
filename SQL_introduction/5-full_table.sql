9d152918fbc76cbe57bb17711a832ec1a7065586
notReuel <northcottreuel@gmail.com>
2024-01-16 15:17:49 +0100

    hh

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,17 +1,5 @@
--- Script to print the full description of the table first_table in hbtn_0c_0 database
+USE hbtn_0c_0;
 
-SELECT
-  CONCAT('CREATE TABLE `', table_name, '` (\n',
-         GROUP_CONCAT(
-           CONCAT('  `', column_name, '` ', column_type,
-                  IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
-                  IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', QUOTE(COLUMN_DEFAULT)), ''),
-                  IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', ''),
-                  ',')
-           SEPARATOR '\n'),
-         IF(CHARACTER_SET_NAME IS NOT NULL, CONCAT(') ENGINE=InnoDB DEFAULT CHARSET=', CHARACTER_SET_NAME, ';'), ')')
-       ) AS CreateTableStatement
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0'
-  AND table_name = 'first_table'
-GROUP BY table_name;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM information_schema.COLUMNS
+WHERE TABLE_NAME = 'first_table';
