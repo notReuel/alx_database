344beede39cde50223ca8ae5b1f1d0e827c39d18
notReuel <northcottreuel@gmail.com>
2024-01-16 14:15:17 +0100

    changed the db name to 'hbtn_test_db_4.first_table'... let see if it works

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- A script to create a table in a selected database
-USE hbtn_test_db_0
-    CREATE TABLE first_table(
+USE hbtn_test_db_4.first_table
+    CREATE TABLE IF NOT EXISTS first_table(
         id INT,
         name VARCHAR(256)
     );
