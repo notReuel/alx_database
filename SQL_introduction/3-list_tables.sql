8b0c44e104d498cba7ea1ef03caa8b05ef96717f
notReuel <northcottreuel@gmail.com>
2024-01-16 13:40:11 +0100

    corrected the errors in the comment, remove the spaces after the '--' apparently the checker was designed to flag that

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,7 +1,7 @@
 -- Script to list the tables in a database
 
---List the databases
+-- List the databases
 SHOW DATABASES;
 
---list the tables in the selected databases
+-- List the tables in the selected databases
 USE mysql SHOW TABLES;
