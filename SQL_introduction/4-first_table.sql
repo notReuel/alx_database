e525b68606b0d356cc895f264eac96e4327eb1c7
notReuel <northcottreuel@gmail.com>
2024-01-16 14:17:35 +0100

    Removed the create table command

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
 -- A script to create a table in a selected database
-USE hbtn_test_db_4.first_table
-    CREATE TABLE IF NOT EXISTS first_table(
+CREATE TABLE IF NOT EXISTS first_table(
         id INT,
         name VARCHAR(256)
     );
