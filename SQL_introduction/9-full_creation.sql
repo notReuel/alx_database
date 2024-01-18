75d85f18019c0f6a97f8bc3047e5ae6fd645b149
notReuel <northcottreuel@gmail.com>
2024-01-18 20:08:14 +0100

    removed some comments

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,6 @@
 -- A script to create another table in a database within my MySQL server with multiple rows
 
--- CREATING THE TABLE
 CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
-
--- INSERTING THE VALUES
 INSERT INTO second_table (id, name, score)
 VALUES  ("1", "John", "10"),
         ("2", "Alex", "3"),
