36d87c5ff1ea0d866df523ef74ff071bd4a360f9
notReuel <northcottreuel@gmail.com>
2024-01-18 14:47:39 +0100

    correct the table name

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- A script that displays the number of records with id=89 in the table first_table of the database hbtn_0c_0 in my MySQL server
-SELECT * FROM tt WHERE id =89;
+SELECT * FROM first_table WHERE id =89;
