09eca9d43533b606b3d3c317033485c22ffc8ecb
notReuel <northcottreuel@gmail.com>
2024-01-18 20:16:45 +0100

    remove the quotes from the int types

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,13 +1,12 @@
 -- A script to create another table in a database within my MySQL server with multiple rows
 
---Creating the tables
 CREATE TABLE second_table (
     id INT, 
     name VARCHAR(256), 
     score INT
-)VALUES  -- adding the values
-    ("1", "John", "10"),
-    ("2", "Alex", "3"),
-    ("3", "Bob", "14"),
-    ("4", "George", "8");   
+) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8); 
  
