7c349699dc86c826704407dacaaf014a161c0311
notReuel <northcottreuel@gmail.com>
2024-01-18 15:55:49 +0100

    Added quotes the int values i.e from 9 to "9"

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -5,8 +5,8 @@ CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
 
 -- INSERTING THE VALUES
 INSERT INTO second_table (id, name, score)
-VALUES  (1, "John", 10),
-        (2, "Alex", 3),
-        (3, "Bob", 14),
-        (4, "George", 8);   
+VALUES  ("1", "John", "10"),
+        ("2", "Alex", "3"),
+        ("3", "Bob", '14"),
+        ("4", "George", "8");   
  
