a51177c8881c51df1e3925e54dd0e03c3663fbec
notReuel <northcottreuel@gmail.com>
2024-01-18 20:14:25 +0100

    Changed the syntax to a single create and values command without using the insert keyword

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,13 @@
 -- A script to create another table in a database within my MySQL server with multiple rows
 
-CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score)
-VALUES  ("1", "John", "10"),
-        ("2", "Alex", "3"),
-        ("3", "Bob", "14"),
-        ("4", "George", "8");   
+--Creating the tables
+CREATE TABLE second_table (
+    id INT, 
+    name VARCHAR(256), 
+    score INT
+)VALUES  -- adding the values
+    ("1", "John", "10"),
+    ("2", "Alex", "3"),
+    ("3", "Bob", "14"),
+    ("4", "George", "8");   
  
