90714252646f29d2cc3fb7923e6551e9631cffbd
notReuel <northcottreuel@gmail.com>
2024-03-28 09:02:04 +0100

    create - v1.0.2

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,13 @@
---Write a script that creates the MySQL server user user_0d_1.
---user_0d_1 should have all privileges on your MySQL server
---The user_0d_1 password should be set to user_0d_1_pwd
---If the user user_0d_1 already exists, your script should not fail
--- creates the MySQL server user user_0d_1 and grant all priviledges
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+-- Write a script that creates the MySQL server user user_0d_1.
+-- user_0d_1 should have all privileges on your MySQL server
+-- The user_0d_1 password should be set to user_0d_1_pwd
+-- If the user user_0d_1 already exists, your script should not fail
+
+-- Create or update user_0d_1 with the desired password
+CREATE OR REPLACE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
